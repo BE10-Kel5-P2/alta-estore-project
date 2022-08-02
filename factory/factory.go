d02d616f0a56471f0c8fafa2cca0f84856879570
Rizki Firdaus <rizki.achanation@gmail.com>
2022-08-02 15:57:56 +0700

    delete init factory

--- factory/factory.go
+++ factory/factory.go
@@ -5,10 +5,6 @@ import (
 	"altaproject2/features/user/delivery"
 	"altaproject2/features/user/usecase"
 
-	pd "altaproject2/features/product/data"
-	pDelivery "altaproject2/features/product/delivery"
-	productCases "altaproject2/features/product/usecase"
-
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -21,11 +17,5 @@ func InitFactory(e *echo.Echo, db *gorm.DB, awsConn *session.Session) {
 	userCase := usecase.New(userData, valid)
 	userHandler := delivery.New(userCase, awsConn)
 	delivery.RouteUser(e, userHandler)
-
-	productData := pd.New(db)
-	valid = validator.New()
-	productCase := productCases.New(productData, valid)
-	productHandler := pDelivery.New(productCase, awsConn)
-	pDelivery.RouteProduct(e, productHandler)
 
 }
