5eea6cf06f057255e11391676ac591b36b126222
Rizki Firdaus <rizki.achanation@gmail.com>
2022-08-02 23:03:27 +0700

    add unit-test product

--- features/product/usecase/logic_test.go
+++ features/product/usecase/logic_test.go
@@ -37,3 +37,49 @@ func TestGetAllUser(t *testing.T) {
 		repo.AssertExpectations(t)
 	})
 }
+
+func TestPostProduct(t *testing.T) {
+	repo := new(mocks.ProductData)
+
+	mockData := domain.Product{ProductName: "Men's bag", Description: "ini adalah tas pria",
+		Price: 200000, ProductPic: "bag.jpg", Stock: 5, Qty: 2}
+	// emptyData := domain.Product{ProductName: "", Description: "",
+	// Price: 0, ProductPic: "", Stock: 0, Qty:0 }
+
+	returnData := mockData
+	returnData.ID = 1
+
+	invalidData := mockData
+	invalidData.ProductName = ""
+
+	noData := mockData
+	noData.ID = 0
+
+	t.Run("Success insert data", func(t *testing.T) {
+		// useCase := New(&mockUserDataTrue{})
+		repo.On("PostItemData", mock.Anything).Return(returnData).Once()
+		useCase := New(repo, validator.New())
+		res := useCase.PostItemAdmin(mockData)
+
+		assert.Equal(t, 200, res)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestUpdateProduct(t *testing.T) {
+	repo := new(mocks.ProductData)
+	mockData := domain.Product{ProductName: "Men's bag", Description: "ini adalah tas pria",
+		Price: 200000, ProductPic: "bag.jpg", Stock: 5, Qty: 2}
+
+	returnData := mockData
+	returnData.ID = 1
+
+	t.Run("Success Update", func(t *testing.T) {
+		repo.On("UpdateItemData", returnData, 1).Return(returnData).Once()
+		useCase := New(repo, validator.New())
+		res := useCase.UpdateItemAdmin(mockData, 1)
+
+		assert.Equal(t, 200, res)
+		repo.AssertExpectations(t)
+	})
+}
