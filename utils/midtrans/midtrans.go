b8878f072a4d2ebfb7fbd00a8bfd0e408efcf3c2
lukmanhafidz <lukmanhafidz40@gmail.com>
2022-08-05 01:45:20 +0700

    konfigurasi midtrans

---
+++ utils/midtrans/midtrans.go
@@ -0,0 +1,34 @@
+package midtrans
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/snap"
+)
+
+func InitConnection() snap.Client {
+	var c = snap.Client{}
+	c.New("SB-Mid-server-RdSR7SzPE67DEllONzb7sCOX", midtrans.Sandbox)
+
+	return c
+}
+
+func CreateConnection(s snap.Client, total, id int) *snap.Response {
+	req := &snap.Request{
+
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  fmt.Sprintf("be10-%d", id),
+			GrossAmt: int64(total),
+		},
+	}
+
+	apiRes, err := s.CreateTransaction(req)
+
+	if err != nil {
+		log.Println("payment err: ", err)
+	}
+
+	return apiRes
+}
